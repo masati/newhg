9b0ec08dee836ca3526b90359fa91aaa21a0158d
vagrant <vagrant@homestead>
2017-02-14 15:14:37 +0000

    Адаптация оформления авторизации

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -20,6 +20,30 @@
         <ul id="navbar" class="menu">
             <li><a href="http://www.heroeswm.ru/">Старая игра</a></li>
             <li><a href="http://dev.heroesaom.com/">Новая игра</a></li>
+            @if (Auth::guest())
+                <li><a href="{{ route('login') }}">Авторизация</a></li>
+                <li><a href="{{ route('register') }}">Регистрация</a></li>
+            @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            @endif
         </ul><!--/.navbar-collapse -->
     </div>
 </nav>
