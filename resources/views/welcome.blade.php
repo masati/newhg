b2633eaef1e8bbd0b17f1f6554e8db7d8c3cf08a
Masati <mmasati@gmail.com>
2017-05-30 00:45:42 +0000

    админка только для авторизованных. логаут со страницы welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,15 @@
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
 
-                        <a href="{{ url('/logout') }}">Logout</a>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
